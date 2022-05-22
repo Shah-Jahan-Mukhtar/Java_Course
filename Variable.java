98318c0240969c28c6added338940857f7fbff36
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-22 13:13:00 -0700

    string in java

---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+  public static void main(String[] args) {
+    
+    //This is Java comment
+
+    String str="Shahjahan"; 
+    int a=5;
+    int b=10;
+    int c=a+b;
+    System.out.println(c);
+    System.out.println(c*2);
+    System.out.println(str);
+
+
+
+
+
+
+  }
+}
