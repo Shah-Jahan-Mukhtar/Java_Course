4fb595d92dd7d9c2b8031f37e5f22e3b468061cd
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-22 13:21:11 -0700

    Variables display

--- Variable.java
+++ Variable.java
@@ -1,15 +1,23 @@
 public class Variable {
   public static void main(String[] args) {
     
-    //This is Java comment
+    //This is Java Variables
 
     String str="Shahjahan"; 
     int a=5;
     int b=10;
     int c=a+b;
+    float flt=1.12f;
     System.out.println(c);
     System.out.println(c*2);
     System.out.println(str);
+
+
+    
+    System.out.println(flt*2+ " "+str);
+
+
+    //Display Variables
 
 
 
