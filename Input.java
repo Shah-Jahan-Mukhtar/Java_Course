3fad62cebef47b238586fff985f61a85f5dfa16a
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-24 20:35:31 -0700

    Input in java

---
+++ Input.java
@@ -0,0 +1,19 @@
+public class Input {
+ 
+    
+    public static void main(String[] args){
+
+
+
+
+        System.out.print("Please enter first value the Values" +" \n" );
+String text=System.console().readLine();
+        System.out.print("Please enter second  the Values" +" \n" );
+
+        String text2=System.console().readLine();
+String addition=text+text2;
+        System.out.println(" The addition of two numbers is :"+addition);
+
+    }
+
+}
