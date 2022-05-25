7ff108a207b4dc0670ff76422996a76880ce25e1
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-24 20:09:22 -0700

    variable declare

---
+++ Datatype.java
@@ -0,0 +1,18 @@
+
+
+public class Datatype {
+    public static void main(){
+        int myNum=12;
+        float myFloat =15.65f;
+        char myChar='D';
+        boolean bool=true;
+        String str="ustad jami";
+
+        
+        Integer a=45;
+        System.out.println(myNum + " "+a+myFloat+" "+myChar +" "+bool+str);
+
+
+        
+    }
+}
--- Variable.java
+++ Variable.java
@@ -9,7 +9,8 @@ public class Variable {
     int b=10;
     int c=a+b;
     float flt=1.12f;
-    
+    int x,y,z;
+    x=y=z=15;
     System.out.println(c);
     System.out.println(c*2);
     System.out.println(str);
@@ -23,6 +24,8 @@ public class Variable {
     //Display Variables
 
     System.out.println(str+"  "+str2);
+
+    System.out.println(x+y+z);
 
 
 
