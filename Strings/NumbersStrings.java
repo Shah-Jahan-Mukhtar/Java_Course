5e17c29773ff296d8b0a3ec015c98ada8238943e
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-26 14:03:31 -0700

    Number and strings

---
+++ Strings/NumbersStrings.java
@@ -0,0 +1,41 @@
+package Strings;
+
+public class NumbersStrings {
+
+    public static void main(String[] args){
+            
+        
+        // Adding Numbers and Strings
+        
+
+// WARNING!
+
+// Java uses the + operator for both addition and concatenation.
+
+// Numbers are added. Strings are concatenated.
+
+// If you add two numbers, the result will be a number:
+//     }
+
+
+int a=454;
+int b=54;
+
+System.out.println(a+b);
+
+
+// If you add two strings, the result will be a string concatenation:
+
+String x="10",y="20";
+
+int z=50;
+
+System.out.println(x+y);
+
+
+// If you add two strings, the result will be a string concatenation:
+
+System.out.println(x+z);
+
+    }
+}
