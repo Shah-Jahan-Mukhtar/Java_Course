80c923db65fc8abd1e0c727fd5487d5251bd6509
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-25 20:17:22 -0700

    operators

--- Operators.java
+++ Operators.java
@@ -9,6 +9,9 @@ int a=45;
 int  b=65;
 System.out.println(++a + " \n" + --b  +" \n" +b%a);
 
+
+
+
     // Assignment operators
 
     int x=65;
@@ -17,8 +20,29 @@ System.out.println(++a + " \n" + --b  +" \n" +b%a);
      x^=2;
 
     System.out.println(x);
+
+
+
     // Comparison operators
-    // Logical operators
+
+
+//     == 	Equal to 	x == y 	
+// != 	Not equal 	x != y 	
+// > 	Greater than 	x > y 	
+// < 	Less than 	x < y 	
+// >= 	Greater than or equal to 	x >= y 	
+// <= 	Less than or equal to 	x <= y
+
+
+
+
+//     // Logical operators
+
+// &&  	Logical and 	Returns true if both statements are true 	x < 5 &&  x < 10 	
+// ||  	Logical or 	Returns true if one of the statements is true 	x < 5 || x < 4 	
+// ! 	Logical not 	Reverse the result, returns false if the result is true 	!(x < 5 && x < 10)
+
+
     // Bitwise operators
 
 
