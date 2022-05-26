485d84717081b397e25e91ae8df146c587a4d5ce
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-25 20:08:04 -0700

    Operators

---
+++ Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+    public static void main(String [] args){
+
+    //     Java divides the operators into the following groups:
+
+    // Arithmetic operators
+    // Assignment operators
+    // Comparison operators
+    // Logical operators
+    // Bitwise operators
+
+
+    
+
+
+    }
+}
