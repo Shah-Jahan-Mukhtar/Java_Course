27aba3b1adb48683b61821d45ad8b7fc90e82181
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-25 20:15:35 -0700

    java operators

--- Operators.java
+++ Operators.java
@@ -4,13 +4,28 @@ public class Operators {
     //     Java divides the operators into the following groups:
 
     // Arithmetic operators
+
+int a=45;
+int  b=65;
+System.out.println(++a + " \n" + --b  +" \n" +b%a);
+
     // Assignment operators
+
+    int x=65;
+    //  x+=x;
+
+     x^=2;
+
+    System.out.println(x);
     // Comparison operators
     // Logical operators
     // Bitwise operators
 
 
-    
+
+
+
+
 
 
     }
