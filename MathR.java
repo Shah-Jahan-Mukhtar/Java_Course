80339fe3128e09aef326a0c74745317e3c9ea0a4
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-27 21:48:59 -0700

    some worl left

--- MathR.java
+++ MathR.java
@@ -20,5 +20,40 @@ int y=Math.min(5, 10);
 
 System.out.println(y);
 
+
+
+// Math.sqrt(x)
+
+// The Math.sqrt(x) method returns the square root of x:
+
+
+int z=Math.sqrt(64);
+
+System.out.println(z);
+
+
+// Math.abs(x)
+
+// The Math.abs(x) method returns the absolute (positive) value of x:
+
+
+Math.abs(-4.7);
+
+
+
+Random Numbers
+
+Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive):
+Example
+
+Math.random();
+
+
+
+To get more control over the random number, e.g. you only want a random number between 0 and 100, you can use the following formula:
+Example
+
+int randomNum = (int)(Math.random() * 101);  // 0 to 100
+
     }
 }
