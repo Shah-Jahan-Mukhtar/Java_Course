472777ae8e23eb6629e806f539ad20faadf4dcd9
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-27 21:46:24 -0700

    Math method

---
+++ MathR.java
@@ -0,0 +1,24 @@
+public class MathR {
+    
+    public static void main(String [] args){
+//         Math.max(x,y)
+
+// The Math.max(x,y) method can be used to find the highest value of x and y:
+
+
+int x=Math.max(5, 10);
+
+System.out.println(x);
+
+
+// Math.min(x,y)
+
+// The Math.min(x,y) method can be used to find the lowest value of x and y:
+
+
+int y=Math.min(5, 10);
+
+System.out.println(y);
+
+    }
+}
