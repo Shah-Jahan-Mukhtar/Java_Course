a902c3aaa30e31744f7cc18298d8218f8ce002dc
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-30 19:40:49 -0700

    Arrays 1d, 2d

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String [] name={"Suleman","Saad"};
+        Integer [][] marks={
+            {10,20,40},
+            {10,20,40,50},
+            {10,30,60,40},
+            {10,30,20,10}
+        };
+
+        System.out.print(marks[2][3]);
+    }
+}
