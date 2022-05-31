af2b86fda446ed9873dd18789cce93f911197181
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-31 13:49:53 -0700

    Arrays method

--- Arrays.java
+++ Arrays.java
@@ -2,8 +2,21 @@ public class Arrays {
     public static void main(String[] args) {
 
         // 1D arrays
-        String [] name={"Suleman","Saad"};
+        String [] name={"Suleman","Saad","Hamayun","Jami"};
 
+
+for(int i = 0;i<name.length;i++){
+    System.out.println(name[i]);
+}
+
+
+// Loop Through an Array with For-Each
+
+// There is also a "for-each" loop, which is used exclusively to loop through elements in arrays:
+
+for(String i :name){
+    System.out.println(i);
+}
 
         // 2D Arrays
         
@@ -15,5 +28,12 @@ public class Arrays {
         };
 
         System.out.print(marks[2][3]);
+
+
+        for(int i=0;i<marks.length;++i){
+            for(int j=0;j<marks[i].length;++j){
+                System.out.println(marks[i][j]);
+            }
+        }
     }
 }
