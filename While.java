6cd8805404e1d8472a8fef935218db786452406a
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-31 14:06:58 -0700

    While loop

---
+++ While.java
@@ -0,0 +1,20 @@
+public class While {
+   public static void main(String[] args) {
+       int i =0;
+       while( i <5)
+       {
+           System.out.println(i);
+           i++;
+       }
+
+
+       int j=0;
+       do{
+        System.out.println(j);
+    j++;
+       }
+    while(i<5);
+   
+    }
+
+}
