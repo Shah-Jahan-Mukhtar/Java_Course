f4cff5825680d9a65e46fc1c16fea4625cd65fdd
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-06-03 18:24:11 -0700

    Hashmap

---
+++ HashM.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class HashM {
+    
+    public static void main(String[] args) {
+        
+        HashMap<String,Integer> hm=new HashMap<String,Integer>();
+
+        hm.put("19014156-030",50);
+        hm.get("19014156-030");
+        System.out.println(hm);
+    }
+}
