9d71ac69c9dbee8b50ca4b97aa776b6ce18baf99
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-06-06 20:00:54 -0700

    ANIMAL CLASS

---
+++ classes/Animal.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Animal {
+ String name;
+String color;
+
+public void Name(String name){
+    this.name=name;
+}
+
+public String Name(){
+    return this.name;
+}
+
+public void makesound()
+{
+    System.out.println("Animals can make sound");
+}
+}
