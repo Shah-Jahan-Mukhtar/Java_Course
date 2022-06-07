e7d70b709f0cbaa75501d30f137779661fa76298
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-06-06 20:12:17 -0700

    Animal class

--- classes/Animal.java
+++ classes/Animal.java
@@ -1,8 +1,20 @@
 package classes;
 
+import javax.print.DocFlavor.STRING;
+
 public class Animal {
  String name;
 String color;
+
+
+public Animal(){
+    this.name="";
+    this.color="";
+}
+
+public Animal(String name){
+    this.name=name;
+}
 
 public void Name(String name){
     this.name=name;
