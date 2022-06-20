46d3481b191f076c8ff02c0d67715694a4cd6255
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-06-19 21:06:36 -0700

    classes

--- ClassAtributes.java
+++ ClassAtributes.java
@@ -1,3 +1,10 @@
+
+import classes.Main;
+
 public class ClassAtributes {
-    
+    public static void main(String[] args) {
+
+        Main myobj = new Main();
+        System.out.println(myobj.i);
+    }
 }
